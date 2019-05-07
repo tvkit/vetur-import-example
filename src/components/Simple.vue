86cdb7a53eba5e4afd8e2a7d847c1facf742dbff
William Byrne <williamb@codics.com>
2019-05-06 22:13:16 -0400

    fix quotes, add Simple class definition

--- src/components/Simple.vue
+++ src/components/Simple.vue
@@ -1,3 +1,12 @@
 <template>
   <h1>No script tag in this component</h1>
 </template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Simple extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
